302343e2418df365ebfa646711fcb3f5f088c91f
qkrwjddn1026 <qkrwjddn1026@naver.com>
2022-09-15 17:22:50 +0900

    java condition and looping example

--- ch04/src/sec3/Loop8.java
+++ ch04/src/sec3/Loop8.java
@@ -10,10 +10,11 @@ public class Loop8 {
  * ........................
  * 2*9=18 ................. 9*9=81
  */
-		for(int i=2;i<10;i++){
-			for(int j=1;j<10;j++){
-				System.out.println("i*j"+i*j);
+		for(int i=1;i<10;i++){
+			for(int j=2;j<10;j++){
+				System.out.print(j+"*"+i+"="+(i*j)+("\t"));
 			}
+			System.out.println();
 		}
 
 	}
