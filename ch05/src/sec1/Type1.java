48982fd0cf07dacb6b1368bb4becc43646d1bbe8
qkrwjddn1026 <qkrwjddn1026@naver.com>
2022-09-16 16:52:34 +0900

    java primitive and reference type

---
+++ ch05/src/sec1/Type1.java
@@ -0,0 +1,27 @@
+package sec1;
+
+public class Type1 {
+
+	public static void main(String[] args) {
+		// Primitive Type(기본형) : 하나의  저장소(변수)에 하나의 데이터만 저장되는 타입 (단일 데이터)
+		// 반드시 사용시에 초기화를 해야 함  
+		// 기본형 자료는 단일 기억만 하므로 스택과 같은 기억장소에 저장된다.
+		// 스택(stack)은 LIFO 구조의 기억장소로 가장 마지막에 입력된 데이터가 가장 먼저 출력될 수 있다. (입구와 출구가 같은 하나의 입출구가 있음) -primitive (기본형 = 단일)
+		// 큐(queue)는 FIFO 구조의 기억장소는 순서대로 입력되고 출력되므로 가장 처음에 입력 된 것이 가장 먼저 출력된다. (입구와 출구가 서로 다름) -reference (참조형 = 다수)
+		
+		// Primitive Type
+		boolean a=true;
+		byte b=120;
+		char c='a';
+		short d=150;
+		int e=150;
+		long f;
+		float g;
+		double h;
+		System.out.println(a);
+		//System.out.println(e); - 변수 e는 초기화 되어 있지 않으므로 사용하지 못함 (int e;)
+		System.out.println(d==e);
+		System.out.println(d!=e);
+		//primitive type 변수는 타입이 달라도 값을 직접 비교함
+	}
+}
---
+++ ch05/src/sec1/Type2.java
@@ -0,0 +1,23 @@
+package sec1;
+
+public class Type2 {
+
+	public static void main(String[] args) {
+		// reference(참조형) type : 여러 개(다수)의 데이터를 저장하기 위한 기억장소로서 null값이 허용됨
+		// 참조형의 종류 : array(배열형), enum(열거형), class(클래스), interface(인터페이스), map(맵), list(리스트), set(셋).....
+		int[] arr1 = {10,20,30};
+		int[] arr2 = {10,20,30};
+		//int[] arr3 = null; (x) reference type은  null값을 허용하나  pointer(히프영역의 주소)가  null이 되어 참조 할 수 없는 예외가 발생됨
+		int [] arr3 = {20,40,60};
+		// int a = null; - primitive type은 null값일 수 없다.
+		// arr3[0] = 20;
+		//System.out.println(arr3[0]);
+		//System.out.println(arr1[1]);
+		System.out.println(arr1);
+		System.out.println(arr2);
+		System.out.println(arr1==arr2);
+		System.out.println(arr1!=arr2);
+		// 참조형은 간접비교를 해야 하므로 직접비교하게 되면, 포인터주소를 비교하게 되므로 서로 다르게 인식 된다.
+		System.out.println(arr3[0]);
+	}
+}
